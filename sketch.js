8c05aa57f950f87ea2932cba5c2b3bf9c5666a8e
Stacey Willcox <kiwimade.sw@gmail.com>
2017-09-10 20:02:51 +1200

    fixed sizing and profile page

--- sketch.js
+++ sketch.js
@@ -1,14 +1,30 @@
-function hvr(dom, action)
-{
-    if (action == 'in')
-    {
-        $(dom).find("[col=g]").css("display", "none");
-        $(dom).find("[col=b]").css("display", "inline-block");
-    }
+function myuploadsbutton() {
+    document.getElementById("myuploads").style.display = "block";
+    document.getElementById("history").style.display = "none";
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("favourites").style.display = "none";
 
-    else
-    {
-        $(dom).find("[col=b]").css("display", "none");
-        $(dom).find("[col=g]").css("display", "inline-block");
-    }
+}
+function historybutton() {
+    document.getElementById("myuploads").style.display = "none";
+    document.getElementById("history").style.display = "block";
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("favourites").style.display = "none";
+
+}
+
+function settingsbutton() {
+    document.getElementById("myuploads").style.display = "none";
+    document.getElementById("history").style.display = "none";
+    document.getElementById("settings").style.display = "block";
+    document.getElementById("favourites").style.display = "none";
+
+}
+
+function favouritesbutton() {
+    document.getElementById("myuploads").style.display = "none";
+    document.getElementById("history").style.display = "none";
+    document.getElementById("settings").style.display = "none";
+    document.getElementById("favourites").style.display = "block";
+
 }
