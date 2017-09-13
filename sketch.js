a0e6ffadc1fec5fcd700653d9c98e8071c149687
Stacey Willcox <kiwimade.sw@gmail.com>
2017-09-13 16:36:52 +1200

    added vr button and page, fixed button colours, added signup option and upload page with form

--- sketch.js
+++ sketch.js
@@ -28,3 +28,55 @@ function favouritesbutton() {
     document.getElementById("favourites").style.display = "block";
 
 }
+
+function editaccount() {
+    document.getElementById("editaccount").style.display = "block";
+    document.getElementById("password").style.display = "none";
+    document.getElementById("privacy").style.display = "none";
+    document.getElementById("statistics").style.display = "none";
+    document.getElementById("restrictions").style.display = "none";
+}
+
+function password() {
+    document.getElementById("editaccount").style.display = "none";
+    document.getElementById("password").style.display = "block";
+    document.getElementById("privacy").style.display = "none";
+    document.getElementById("statistics").style.display = "none";
+    document.getElementById("restrictions").style.display = "none";
+}
+
+function privacy() {
+    document.getElementById("editaccount").style.display = "none";
+    document.getElementById("password").style.display = "none";
+    document.getElementById("privacy").style.display = "block";;
+    document.getElementById("statistics").style.display = "none";
+    document.getElementById("restrictions").style.display = "none";
+}
+
+
+function statistics() {
+    document.getElementById("editaccount").style.display = "none";
+    document.getElementById("password").style.display = "none";
+    document.getElementById("privacy").style.display = "none";
+    document.getElementById("statistics").style.display = "block";
+    document.getElementById("restrictions").style.display = "none";
+}
+
+function restrictions() {
+    document.getElementById("editaccount").style.display = "none";
+    document.getElementById("password").style.display = "none";
+    document.getElementById("privacy").style.display = "none";
+    document.getElementById("statistics").style.display = "none";
+    document.getElementById("restrictions").style.display = "block";
+}
+
+function signup() {
+    document.getElementById("login-container").style.display = "none";
+    document.getElementById("signup-container").style.display = "block"; 
+}
+
+function login() {
+    document.getElementById("login-container").style.display = "block";
+    document.getElementById("signup-container").style.display = "none"; 
+}
+
